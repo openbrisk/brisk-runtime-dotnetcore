87aeb6ba1dc12f6c01156dab607efaa0f063593b
Matthias Gernand <m.gernand@fluxera.com>
2018-01-29 19:35:36 +0100

    Added swagger docs.

--- src/OpenBrisk.Runtime/Controllers/FunctionController.cs
+++ src/OpenBrisk.Runtime/Controllers/FunctionController.cs
@@ -1,80 +1,77 @@
 ﻿namespace OpenBrisk.Runtime.Controllers
 {
-    using System.Dynamic;
-    using System.IO;
+	using System.Dynamic;
+	using System.IO;
 	using System.Text;
 	using System.Threading.Tasks;
 	using Microsoft.AspNetCore.Mvc;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
-    using OpenBrisk.Runtime.Core.Interfaces;
-    using OpenBrisk.Runtime.Core.Models;
+	using Newtonsoft.Json;
+	using Newtonsoft.Json.Linq;
+	using OpenBrisk.Runtime.Core.Interfaces;
+	using OpenBrisk.Runtime.Core.Models;
 
-    [Route("/")]
-    public class FunctionController : Controller
-    {
-        private readonly IFunction function;
-        private readonly IInvoker invoker;
+	[Route("runtime/v1")]
+	public class FunctionController : Controller
+	{
+		private readonly IFunction function;
+		private readonly IInvoker invoker;
 
-        public FunctionController(IFunction function, IInvoker invoker)
-        {
-            this.function = function;
-            this.invoker = invoker;
-        }
+		public FunctionController(IFunction function, IInvoker invoker)
+		{
+			this.function = function;
+			this.invoker = invoker;
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Post()
-        {
-            // string data;
-            // using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
-            // {  
-            //     data = await reader.ReadToEndAsync();
-            // }
+		[HttpPost]
+		public async Task<IActionResult> Post()
+		{
+			// string data;
+			// using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+			// {  
+			//     data = await reader.ReadToEndAsync();
+			// }
 
-            using (StreamReader streamReader = new StreamReader(this.Request.Body, Encoding.UTF8))
-            using (JsonReader reader = new JsonTextReader(streamReader))
-            {
-                JsonSerializer serializer = new JsonSerializer();
+			using (StreamReader streamReader = new StreamReader(this.Request.Body, Encoding.UTF8))
+			using (JsonReader reader = new JsonTextReader(streamReader))
+			{
+				JsonSerializer serializer = new JsonSerializer();
 
-                BriskContext context = new BriskContext
-                {
-                    Data = serializer.Deserialize(reader),
-                };
+				BriskContext context = new BriskContext
+				{
+					Data = serializer.Deserialize(reader),
+				};
 
-                object result = await this.invoker.Execute(this.function, context);
+				object result = await this.invoker.Execute(this.function, context);
 
-                return this.GetSuitableActionResult(result);
-            }
-        }
+				return this.GetSuitableActionResult(result);
+			}
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Get() 
-        {
-            BriskContext context = new BriskContext
-            {
-                Data = new {}
-            };
+		[HttpGet]
+		public async Task<IActionResult> Get()
+		{
+			BriskContext context = new BriskContext
+			{
+				Data = new { }
+			};
 
-            object result = await this.invoker.Execute(this.function, context);
-            return this.GetSuitableActionResult(result);
-        }
+			object result = await this.invoker.Execute(this.function, context);
+			return this.GetSuitableActionResult(result);
+		}
 
-        [HttpGet("/healthcheck")]
-        public IActionResult Health() => this.Ok();    
+		private IActionResult GetSuitableActionResult(object result)
+		{
+			if (result is string)
+			{
+				return this.Ok(result);
+			}
 
-        private IActionResult GetSuitableActionResult(object result)
-        {
-            if(result is string) 
-            {
-                return this.Ok(result);
-            }
+			if (result == null)
+			{
+				return this.Ok();
+			}
 
-            if(result == null)
-            {
-                return this.Ok();
-            }
-
-            return this.Json(result);
-        } 
-    }
+			return this.Json(result);
+		}
+	}
 }
---
+++ src/OpenBrisk.Runtime/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenBrisk.Runtime.Controllers
+{
+	[Route("/")]
+	public class HealthController : Controller
+	{
+		[HttpGet("healthz")]
+		public IActionResult Health() => this.Ok();
+	}
+}
