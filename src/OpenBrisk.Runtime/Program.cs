5d5165bc8c0ca23bf44c9fb67fc3d03f237b595a
Matthias Gernand <m.gernand@fluxera.com>
2018-07-25 17:05:09 +0200

    Integrated core project in to runtime project.

--- src/OpenBrisk.Runtime/Program.cs
+++ src/OpenBrisk.Runtime/Program.cs
@@ -1,22 +1,20 @@
 ﻿namespace OpenBrisk.Runtime
 {
-    using System;
-    using Microsoft.AspNetCore;
-    using Microsoft.AspNetCore.Hosting;
+	using Microsoft.AspNetCore;
+	using Microsoft.AspNetCore.Hosting;
 
-	public class Program
+	public static class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls($"http://+:8080")
-                .Build();
-        }
+	    private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+	    {
+		    return WebHost.CreateDefaultBuilder(args)
+		                  .UseStartup<Startup>()
+		                  .UseUrls($"http://+:8080");
+	    }
     }
 }
--- src/OpenBrisk.Runtime/Startup.cs
+++ src/OpenBrisk.Runtime/Startup.cs
@@ -2,43 +2,49 @@
 {
 	using Microsoft.AspNetCore.Builder;
 	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.AspNetCore.Mvc;
 	using Microsoft.Extensions.Configuration;
 	using Microsoft.Extensions.DependencyInjection;
 	using OpenBrisk.Runtime.Core.Interfaces;
 	using OpenBrisk.Runtime.Core.Models;
 
 	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			this.Configuration = configuration;
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+	        // Compile the function.
+	        //string requirementsPath = this.Configuration["Compiler:RequirementsPath"];
+	        IFunction function = FunctionFactory.BuildFunction(this.Configuration);
+
+	        services.AddSingleton<IFunction>(function);
+	        services.AddSingleton<IFunctionSettings>(serviceProvider => serviceProvider.GetService<IFunction>().FunctionSettings);
+	        services.AddSingleton<IInvoker>(new DefaultInvoker());
 		}
 
-		public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //else
+            //{
+            //    app.UseHsts();
+            //}
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddMvc();
-
-			// Compile the function.
-			//string requirementsPath = this.Configuration["Compiler:RequirementsPath"];
-			IFunction function = FunctionFactory.BuildFunction(this.Configuration);
-
-			services.AddSingleton<IFunction>(function);
-			services.AddSingleton<IFunctionSettings>(serviceProvider => serviceProvider.GetService<IFunction>().FunctionSettings);
-			services.AddSingleton<IInvoker>(new DefaultInvoker());
-		}
-
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-
-			app.UseMvc();
-		}
-	}
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
 }
